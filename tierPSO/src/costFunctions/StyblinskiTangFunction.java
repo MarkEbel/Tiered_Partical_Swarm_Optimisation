4e8fd2181f09306ca9dba7d46e50652e8f0817b8
MarkEbel <44725968+MarkEbel@users.noreply.github.com>
2021-03-11 08:40:17 +0000

    styblinskiTang Class completed

--- tierPSO/src/costFunctions/StyblinskiTangFunction.java
+++ tierPSO/src/costFunctions/StyblinskiTangFunction.java
@@ -1,18 +1,33 @@
 package costFunctions;
 
+import java.util.Random;
+
 public class StyblinskiTangFunction implements costMethods{
 
 	@Override
-	public double evaluate(double[] position) {
-		return 0;
-		// TODO Auto-generated method stub
-		
+	public double evaluate(double[] position) {double tmp = 0.0;
+		for(int i =0; i < position.length; i++) {
+			tmp += (Math.pow(position[i], 4) - 16*(Math.pow(position[i], 2)) +5*position[i]);
+		}
+		tmp = 0.5*tmp;
+		return tmp;
 	}
+	
 
+	private Random random = new Random();
 	@Override
 	public double[] randomSolution() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		double[] tmp = new double[2];
+		tmp[0] = random.nextDouble() + random.nextInt(4);
+		tmp[1] = random.nextDouble() + random.nextInt(4);
+	
+		if(random.nextBoolean()) {
+			tmp[0] = -tmp[0];
+		}
+		if(random.nextBoolean()) {
+			tmp[1] = -tmp[1];
+		}
+			return tmp;
+		}
 
 }
